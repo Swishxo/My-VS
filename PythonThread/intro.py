a06b06e8a49fa7aa6f28a6acec24be430758d25b
Stephon Tidd <stidd@mail.valenciacollege.edu>
2022-05-07 10:42:09 -0400

    update

--- PythonThread/intro.py
+++ PythonThread/intro.py
@@ -1,4 +1,5 @@
 # first import thread
+from concurrent.futures import thread
 import threading
 
 # create a function for the thread to target
@@ -12,3 +13,13 @@ t1 = threading.Thread(target=hi)
 # must use the start() function on thread obj to run code in function
     # YOU DO NOT make a function call on itself 
 t1.start()
+
+# without comments
+
+def zipcode():
+    print("12345")
+
+t2 = threading.Thread(target=zipcode)
+t2.start()
+
+
