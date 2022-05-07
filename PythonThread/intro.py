b12344ad52b5cf71c56b98a3237baa149c934a67
Stephon Tidd <stidd@mail.valenciacollege.edu>
2022-05-07 10:16:50 -0400

    added comments

--- PythonThread/intro.py
+++ PythonThread/intro.py
@@ -5,6 +5,10 @@ import threading
 def hi():
     print("Hi")
 
+# create a thread obj
+    #target: controls a specified function
 t1 = threading.Thread(target=hi)
 
+# must use the start() function on thread obj to run code in function
+    # YOU DO NOT make a function call on itself 
 t1.start()
